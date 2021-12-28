59b55e3b2f6aaab5dc4be0fb9f181fa02935d199
jawadabbass <jawadabbass@hotmail.com>
2021-12-28 20:16:56 +0500

    route_name_prefix

--- config/jawad_permission_uuid.php
+++ config/jawad_permission_uuid.php
@@ -3,7 +3,7 @@
 return [
     'route_middleware' => ['web', 'jpu_is_super_admin'],
     'route_prefix' => 'admin',
-    'route_name' => 'admin.',
+    'route_name_prefix' => 'admin.',
     'route_domain' => null,
     'guard' => null,
 ];
